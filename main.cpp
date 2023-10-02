c00a18a33c3bdfc1f5fc3038999743f1f9be8dec
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-10-02 12:23:22 +0900

    01_02

--- main.cpp
+++ main.cpp
@@ -1,28 +1,26 @@
 ﻿#include <stdio.h>
 
-template <typename Type>
-Type Min(Type a, Type b)
+int calc(int hour,int money)
 {
-	return a < b ? a : b;
-}
-
-char* Min(char a,char b)
-{
-	static char string[] = "数字以外は代入できません";
-	printf("%s\n",string);
-	return string;
+	money = money * 2 - 50;
+	if (hour <= 1)
+	{
+		return money;
+	}
+	return money + calc(hour-1,money);
 }
 
 int main() {
 
-	int int1 = 2, int2 = 5;
-	float float1 = 1.0f, float2 = -1.2f;
-	double double1 = 0.5, double2 = 2.8;
-	char char1 = 'a', char2 = 'b';
-	printf("%d\n", Min(int1, int2));
-	printf("%f\n", Min(float1, float2));
-	printf("%lf\n", Min(double1, double2));
-	//printf("%s\n", Min(char1, char2));
-	Min(char1, char2);
+	int hour = 1;
+	int money = 100;
+	do
+	{
+		hour++;
+		money =(100 +  calc(hour-1, 100))/hour;
+		printf("%d時間 ,平均: %d円\n",hour,money);
+	} while (1072 > money);
+	printf("%d時間働くと再帰的な資金体系のほうが儲かる\n",hour);
+
 	return 0;
 }
