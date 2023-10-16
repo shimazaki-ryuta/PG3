33fb0493a4e3003451c812659be7adc740fc6750
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-10-16 10:24:21 +0900

    01_04

--- main.cpp
+++ main.cpp
@@ -2,40 +2,37 @@
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
+#include <functional>
 
 
-void ReturnAnswer(int &in)
-{
-	int answer = rand() % 6 + 1;
-	printf("%d",answer);
-	if (answer % 2 && !(in))
-	{
-		printf(" 結果 : 当たり\n");
-		return;
-	}
-	printf(" 結果 : はずれ\n");
-	return;
-}
-
-void TimedCall(void (* pFunc)(int &in),int &input, int second)
+void TimedCall(std::function <void(int)> func, int& input, int second)
 {
 	for (second; second >0; second--)
 	{
 		printf("*\n");
 		Sleep(1000);
 	}
-	pFunc(input);
+	func(input);
 }
 
 int main() {
 	int input = 0;
 	srand(time(NULL));
 	
-	void (*pFunc)(int&);
-	pFunc = ReturnAnswer;
+	std::function<void(int)> func = [](int input){
+		int answer = rand() % 6 + 1;
+		printf("%d", answer);
+		if (answer % 2 == (input))
+		{
+			printf(" 結果 : はずれ\n");
+			return;
+		}
+		printf(" 結果 : 当たり\n");
+		return;
+	};
 
 	printf("半...0 丁...1 ");
 	scanf_s("%d",&input);
-	TimedCall(pFunc,input,3);
+	TimedCall(func,input,3);
 	return 0;
 }
