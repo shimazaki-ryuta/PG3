2e1924edc73df31b829efade8d9b5121c81ee2ee
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-12-11 11:13:52 +0900

    05_01

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <stdio.h>
-
+#include <algorithm>
+#include <list>
 int main() {
-	printf("あいうえお\n");
+	std::list<const char*>yamanoteLine{"Tokyo","Kanda","Akihanara","Okatimati","Ueno","Uguisudani",
+		"Nippori","Tabata","Komairi","Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo",
+		"Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatsucho",
+	"Shimbashi","Yurakucho"};
+	std::list<const char*>::iterator yamanoteLineIterator = yamanoteLine.begin();
+	int num = 0;
+	printf("1970\n");
+	for (std::list<const char*>::iterator iterator = yamanoteLine.begin(); iterator != yamanoteLine.end(); iterator++) {
+		printf("%02d,%s\n",++num,*iterator);
+	}
+	yamanoteLineIterator = std::find(yamanoteLine.begin(), yamanoteLine.end(),"Tabata");
+	if (yamanoteLineIterator != yamanoteLine.end()) {
+		yamanoteLine.insert(yamanoteLineIterator,"Nishi-Nippori");
+	}
+	printf("\n2019\n");
+	num = 0;
+	for (std::list<const char*>::iterator iterator = yamanoteLine.begin(); iterator != yamanoteLine.end(); iterator++) {
+		printf("%02d,%s\n", ++num, *iterator);
+	}
+	yamanoteLineIterator = std::find(yamanoteLine.begin(), yamanoteLine.end(), "Tamachi");
+	if (yamanoteLineIterator != yamanoteLine.end()) {
+		yamanoteLine.insert(yamanoteLineIterator, "TakanawaGateWay");
+	}
+	printf("\n2022\n");
+	num = 0;
+	for (std::list<const char*>::iterator iterator = yamanoteLine.begin(); iterator != yamanoteLine.end(); iterator++) {
+		printf("%02d,%s\n", ++num, *iterator);
+	}
 	return 0;
 }
